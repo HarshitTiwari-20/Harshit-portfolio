a7366d72516c9b29d210d3ce981017a2e227dbf7
HarshitTiwari-20 <ht337378@gmail.com>
2026-01-15 23:01:33 +0530

    2nd commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
 import { styles } from "../styles"
 import { useState } from "react"
-import logo from "../assets/logo.svg"
+import {logo, menu, close} from "../assets"
 import { navLinks } from "../constants"
 const Navbar = () => {
-  const [active, setActive] = useState("")
+  const [active, setActive] = useState("");
+  const [toggle, settoggle] = useState(false);
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -17,9 +18,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <div className="text-white text-[26px] font-bold cursor-pointer">
+          <div className="text-white text-[26px] font-bold cursor-pointer ">
             <span>Harshit | </span>
-            <span className="sm:block hidden ">Portfolio</span>
+            <span className=" ">Portfolio</span>
           </div>
 
         </Link>
@@ -36,6 +37,30 @@ const Navbar = () => {
           ))}
 
         </ul>
+        <div className="flex sm:hidden flex flex-1 justify-end items-center">
+          <img 
+            src={toggle? close : menu} 
+            alt="menu" 
+            className=" w-[28px] h-[28px] object-pointer"
+            onClick={() => settoggle(!toggle)}
+            />
+
+            <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0  mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+              <ul className="list-none flex flex-col gap-4 items-start justify-end">
+                {navLinks.map((nav) => (
+                  <li
+                    key={nav.id}
+                    className={`${active === nav.title ? "text-white" : "text-secondary"
+                      } hover:text-white text-[18px] font-medium cursor-pointer`}
+                    onClick={() => setActive(nav.title)}
+                  >
+                    <a href={`#${nav.id}`}>{nav.title}</a>
+                  </li>
+                ))}
+              </ul> 
+            </div>
+        </div>
+
 
       </div>
     </nav>
