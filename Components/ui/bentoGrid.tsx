5b1247d1fa2dacb730697e083cffa5571f4b236b
HarshitTiwari-20 <ht337378@gmail.com>
2026-01-29 23:47:11 +0530

    added gradient-animation

--- Components/ui/bentoGrid.tsx
+++ Components/ui/bentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/libs/utils";
+import { BackgroundGradientAnimation } from "./gradient-animetion";
 
 export const BentoGrid = ({
   className,
@@ -44,6 +45,7 @@ export const BentoGridItem = ({
 
 }) => {
   return (
+
     <div
       className={cn(
         "group/bento relative shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-3xl border  bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none border-blue-600",
@@ -51,11 +53,23 @@ export const BentoGridItem = ({
       )}
       style={{
         background: 'rgb(4,7,29)',
-        backgroundColor: 'gradient...',
-
+        backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(59,59,68,1) 26%, rgba(93,108,111,1) 100%',
       }}
     >
-      {header}
+
+      <div className=" {`${id === 6} && 'flex justify-center h-full'`}">
+        <div className="w-full h-full absolute">
+          <img src={img} alt={img} className={cn(imgClassName,'object-cover, object-center')}/>
+        </div>
+        <div className={`absolute right-0 -bottom-5 ${id ===5 && 'w-full opacity-80'}`}>
+          {spareImg && (
+            <img src={spareImg} alt={spareImg} className="w-full h-full object-cover, object-center"/>
+          )}
+        </div>
+        {id === 6 && (
+          <BackgroundGradientAnimation/>
+        )}
+      </div>
       <div className="transition duration-200 group-hover/bento:translate-x-2">
         {icon}
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
