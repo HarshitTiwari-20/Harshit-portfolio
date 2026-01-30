395e76093fb5ef58c1d5f6d070423e4b69f23b80
HarshitTiwari-20 <ht337378@gmail.com>
2026-01-30 22:07:23 +0530

    added globe

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -11,24 +11,24 @@ export default function Hero() {
                 <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
                 <Spotlight className="-top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
                 <Spotlight className="-top-28 left-80 h-[80vh] w-[80vw]" fill="blue" />
-               
+
             </div>
             <div className="absolute top-0 left-0 flex h-screen w-full items-center justify-center bg-white dark:bg-black-100  dark:bg-grid-white/[0.02] bg-grid-black-100/[0.02]">
-           
-     
-            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-            
+
+
+                <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+
             </div>
 
             <div className=" flex justify-center relative my-20 z-10 ">
                 <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-                    <h2 className=" uppercase tracking-widest  text-center text-blue-100 mx-w-80  text-2xl underline">
+                    <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 underline">
                         Hi, I&apos;m Harshit
                     </h2>
-                   
+
                     <TextGenerateEffect
                         words="Building Modern Web & System-Level Solutions"
-                        className="text-center text-2xl md:text-2xl lg:text-4xl pb-1"
+                        className="text-center text-[40px] md:text-5xl lg:text-6xl"
                     />
 
                     <p className="text-center py-10 md:tracking-wider font-bold md:text-xl lg:text-2xl">
@@ -36,18 +36,18 @@ export default function Hero() {
                     </p>
                 </div>
             </div>
-            
+
 
             <a href="#about" className="flex justify-center">
                 <MagicButton
                     title="My Projects"
                     position="right"
                     icon={<FaLocationArrow />}
-                    
+
                 />
             </a>
-            
-            
+
+
         </div>
     )
 }
--- Components/ui/bentoGrid.tsx
+++ Components/ui/bentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/libs/utils";
 import { BackgroundGradientAnimation } from "./gradient-animetion";
+import { Globe } from "./globe";
 
 export const BentoGrid = ({
   className,
@@ -11,7 +12,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
+        "mx-auto grid max-w-7xl gap-4 md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 lg:grid-cols-5",
         className,
       )}
     >
@@ -52,23 +53,34 @@ export const BentoGridItem = ({
         className,
       )}
       style={{
-        background: 'rgb(4,7,29)',
-        backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(59,59,68,1) 26%, rgba(93,108,111,1) 100%',
+         background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
 
-      <div className=" {`${id === 6} && 'flex justify-center h-full'`}">
+      <div className={`${id === 6 && 'flex justify-center h-full'}`}>
         <div className="w-full h-full absolute">
-          <img src={img} alt={img} className={cn(imgClassName,'object-cover, object-center')}/>
+          <img src={img} alt={img} className={cn(imgClassName, 'object-cover, object-center')} />
         </div>
-        <div className={`absolute right-0 -bottom-5 ${id ===5 && 'w-full opacity-80'}`}>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
           {spareImg && (
-            <img src={spareImg} alt={spareImg} className="w-full h-full object-cover, object-center"/>
+            <img src={spareImg} alt={spareImg} className="w-full h-full object-cover, object-center" />
           )}
         </div>
         {id === 6 && (
-          <BackgroundGradientAnimation/>
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+          </BackgroundGradientAnimation>
         )}
+        <div className={cn(
+          titleClassName,
+          ' group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+        )}>
+          <div className="font-sans font-extralight text-[#1c2d3] text-sm md:text-xs lg:text-base ">
+            {description}
+          </div>
+        </div>
       </div>
       <div className="transition duration-200 group-hover/bento:translate-x-2">
         {icon}
@@ -78,7 +90,13 @@ export const BentoGridItem = ({
         <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
           {description}
         </div>
+        <div
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+          >
+            {title}
+          </div>
       </div>
+      { id === 2 && <GridGlobe />}
     </div>
   );
 };
